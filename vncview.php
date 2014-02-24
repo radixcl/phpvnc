842f45738d15d64f3cffb107807a573877596a43
Matias Fernandez <matias.fernandez@gmail.com>
2014-02-23 23:19:53 -0300

    improved keyboard engine

--- vncview.php
+++ vncview.php
@@ -190,73 +190,30 @@ $sesid = $_COOKIE['PHPSESSID'];
 	mouseX = (e.clientX - offset.left);
 	mouseY = (e.clientY - offset.top);
   });
-
+  
+  // keyboard map
+  var keyMap = new Array();
+  keyMap[8] = [0xff, 8];		// backspace
+  keyMap[13] = [0xff, 13];		// enter
+  keyMap[16] = [0xff, 0xe1];	// shift
+  keyMap[17] = [0xff, 0xe3];	// control
+  keyMap[18] = [0xff, 0xe9];	// alt
+  keyMap[27] = [0xff, 0x1b];	// esc
+  keyMap[37] = [0xff, 0x51];	// left arrow
+  keyMap[38] = [0xff, 0x52];	// up arrow
+  keyMap[39] = [0xff, 0x53];	// right arrow
+  keyMap[40] = [0xff, 0x54];	// down arrow
+  keyMap[188] = [0x00, 0x2c];	// comma
+  keyMap[222] = [0x00, 0x22];	// "
+  keyMap[191] = [0x00, 0x2f];	// slash
+  
 	function keyPress(e, upDown) {
 	  var keyCode = e.keyCode;
 	  var spKey = 0;
 	  var retcode = true;
 	  
-	  console.log('keyPress(' + upDown + '); keycode ' + keyCode);
+	  console.log('keyPress(' + upDown + '); keycode ' + keyCode + ' ('  + hex8(keyCode) + ') ' + e.charCode);
 	  
-	  // specials
-	  if (keyCode == 8) { // backspace
-		spKey = 0xff;
-		retcode = false;
-	  }
-	  
-	  if (keyCode == 13) { // enter
-		spKey = 0xff;
-		retcode = false;
-	  }
-	  	  
-	  if (keyCode == 16) { // left shift
-		spKey = 0xff;
-		keyCode = 0xe1;
-		retcode = false;
-	  }
-	  
-	  if (keyCode == 17) { //control
-		spKey = 0xff;
-		keyCode = 0xe3;
-		retcode = false;
-	  }
-
-	  if (keyCode == 17) { // left alt
-		spKey = 0xff;
-		keyCode = 0xe9;
-		retcode = false;
-	  }
-
-	  if (keyCode == 37) { // left
-		spKey = 0xff;
-		keyCode = 0x51;
-		retcode = false;
-	  }
-
-	  if (keyCode == 38) { // up
-		spKey = 0xff;
-		keyCode = 0x52;
-		retcode = false;
-	  }
-
-	  if (keyCode == 39) { // right
-		spKey = 0xff;
-		keyCode = 0x53;
-		retcode = false;
-	  }
-
-	  if (keyCode == 40) { // down
-		spKey = 0xff;
-		keyCode = 0x54;
-		retcode = false;
-	  }
-
-	  if (keyCode == 27) { // esc
-		spKey = 0xff;
-		keyCode = 0x1b;
-		retcode = false;
-	  }
-
 	  if (keyCode >= 112 && keyCode <= 123) { // F1-F12
 		var fBase = 0xbe-1;
 		var cBase = 112-1;
@@ -267,6 +224,12 @@ $sesid = $_COOKIE['PHPSESSID'];
 		keyCode = fBase + fCode;
 		retcode = false;
 	  }
+	  // process keymap
+	  else if (typeof(keyMap[keyCode]) != 'undefined') {
+		spKey = keyMap[keyCode][0];
+		keyCode = keyMap[keyCode][1];
+		retcode = false;
+	  } 	  
 	  
 	  var bytes;
 	  bytes = [0x04, upDown, 0x00, 0x00, 0x00, 0x00, spKey, keyCode];
